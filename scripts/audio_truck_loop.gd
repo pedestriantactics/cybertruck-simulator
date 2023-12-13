dad114a9effeafda6269ef3ecd705cf6f0feaa7a
Dan <hi@pedestriantactics.com>
2023-12-12 23:06:43 -0800

    tire noises, sound fixes

--- scripts/audio_truck_loop.gd
+++ scripts/audio_truck_loop.gd
@@ -6,6 +6,9 @@ extends AudioStreamPlayer
 # this only happens when the acceleration key is down
 # the volume is capped at 0db
 
+@export var forward_stream: AudioStream
+@export var reverse_stream: AudioStream
+
 @onready var parent = get_parent()
 
 # Called when the node enters the scene tree for the first time.
@@ -21,6 +24,12 @@ func _process(delta):
 	if !playing and volume_db > -70:
 		play()
 
+	# if the parent is moving forward, play the forward stream
+	if parent.forward && parent.is_moving_forward() and stream != forward_stream:
+		stream = forward_stream
+	elif !parent.forward && !parent.is_moving_forward() and stream != reverse_stream:
+		stream = reverse_stream
+
 	if Input.get_axis("move_backward", "move_forward") <= 0:
 		set_volume_db(-80)
 		return
@@ -32,7 +41,8 @@ func _process(delta):
 	# get the length of the velocity vector
 	var speed = velocity.length()
 	# scale the speed so that 0 is -80 and 100 is 0
-	speed = lerp(-80, 0, speed / 10)
+	# speed = lerp(-80, 0, speed / 10)
+	speed = (velocity.length() * 10) - 80
 	# cap the volume at 0
 	if speed > 0:
 		speed = 0
---
+++ scripts/audio_truck_tires_loop.gd
@@ -0,0 +1,51 @@
+extends AudioStreamPlayer
+
+# the tire screech is playing on loop by default
+# this will turn the volume up if there's a lot of lateral movement
+
+@onready var parent = get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_volume_db(-80)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# if below a volume stop playing at all
+	# if playing and volume_db < -70:
+	# 	stop()
+	# if !playing and volume_db > -70:
+	# 	play()
+	
+	if parent == null:
+		return
+
+	# get the velocity of the parent object
+	var velocity = parent.get_linear_velocity()
+
+	# if the velocity is below a certain threshold or the parent is not moving forward
+	if velocity.length() < 4 or !parent.is_moving_forward():
+		# set the volume to 0
+		set_volume_db(-80)
+		return
+
+	# set the volume of the tire screech based on the skid of the wheels
+	# get the wheels by getting nodes FL, FR, BL, BR which are siblings
+	var wheels = []
+	for child in parent.get_children():
+		if child.name == "FL" or child.name == "FR" or child.name == "BL" or child.name == "BR":
+			wheels.append(child)
+	var skid = 4
+	for wheel in wheels:
+		skid -= wheel.get_skidinfo()
+		# if the wheel is moving laterally, add to the skid
+	# divide the skid by the number of wheels
+	skid /= 4
+
+	var calculated_volume = -80 + (skid * 180)
+	if calculated_volume > 0:
+		calculated_volume = 0
+
+	# set the volume based on the skid
+	set_volume_db(calculated_volume)
