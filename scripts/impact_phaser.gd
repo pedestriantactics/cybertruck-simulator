f141b897ade24403450a69357299af903e241d52
Dan <hi@pedestriantactics.com>
2023-12-27 20:39:00 -0800

    basic phaser test

---
+++ scripts/impact_phaser.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# get the audio effect phaser from the bus named "Spatial"
+@onready var phaser = get_node("Spatial/Phaser")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# get the parent node and connect the signal 
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
