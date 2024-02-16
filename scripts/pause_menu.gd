293546b67d073dfd47e0cd62a4df47a5e20fde5d
Dan <hi@pedestriantactics.com>
2024-02-16 10:32:03 -0800

    pause menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,12 +1,57 @@
 extends Control
 
+@onready var scene_changer = get_node("/root/SceneChanger")
+
+@export var resume_button_path: NodePath
+var resume_button: Button
+
+@export var restart_button_path: NodePath
+
+@export var main_menu_button_path: NodePath
+
+# hides when paused
+@export var make_invisible_when_paused_paths: Array[NodePath]
+var make_invisible_when_paused: Array[Node]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	# get the child button named resume and hook it
+
+	for path in make_invisible_when_paused_paths:
+		var node = get_node(path)
+		make_invisible_when_paused.append(node)
+
+	resume_button = get_node(resume_button_path)
+	resume_button.pressed.connect(_resume)
+
+	var restart_button = get_node(restart_button_path)
+	restart_button.pressed.connect(scene_changer.change_scene.bind("play"))
+
+	var main_menu_button = get_node(main_menu_button_path)
+	main_menu_button.pressed.connect(scene_changer.change_scene.bind("menu"))
+
+	visible = false
 	
-# when the escape key is pressed the game will pause
-func 
+
+# when the key is pressed
+func _input(event):
+	if event.is_action_released("pause"):
+
+		if visible:
+			_resume()
+		else:
+			if make_invisible_when_paused.size() > 0:
+				for item in make_invisible_when_paused:
+					item.hide()
+			visible = true
+			resume_button.grab_focus()
+			get_tree().paused = true
+
+func _resume():
+	if make_invisible_when_paused.size() > 0:
+		for item in make_invisible_when_paused:
+			item.show()
+	visible = false
+	get_tree().paused = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
