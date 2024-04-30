cecbb18760e1745eba13f60c1d04fbfaabaf04d0
Nick Glenn <nick@catalystsoftworks.com>
2024-04-30 15:35:04 -0700

    Timer tick effect and volume slider now plays sounds when passing step pips

--- scripts/volume_slider.gd
+++ scripts/volume_slider.gd
@@ -1,14 +1,31 @@
 extends HSlider
 
 @onready var globals = $"/root/Globals"
+@onready var audio_player = $"./AudioStreamPlayer"
+
+# random sound effects to choose from
+var sounds: Array
+
+var current_step: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	min_value = 0
 	max_value = 1
-	step = 0.001
+	step = 0.1
 	value = globals.master_volume
+	current_step = int(value * 10)
 	value_changed.connect(_on_value_changed)
 
+	for i in range(1, 3):
+		sounds.append(load("res://sounds/truck-crash-" + str(i) + ".wav"))
+
 func _on_value_changed(new_value: float) -> void:
-	globals.master_volume = new_value
+	var next_step = int(new_value * 10)
+	if next_step != current_step:
+		current_step = next_step
+		globals.master_volume = new_value
+		
+		# pick a random sound effect
+		audio_player.stream = sounds[randi() % sounds.size()]
+		audio_player.play()
