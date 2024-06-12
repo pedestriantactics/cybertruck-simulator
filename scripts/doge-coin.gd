1d52dd02849837369333ebe65b8164a40d1abc6f
Dan <hi@pedestriantactics.com>
2024-06-11 21:18:25 -0700

    syncing up edits with doge coin

--- scripts/doge-coin.gd
+++ scripts/doge-coin.gd
@@ -12,10 +12,12 @@ func _ready():
 func _physics_process(delta):
 	# check if the area3D is colliding
 	var bodies = area.get_overlapping_bodies()
-	if bodies != null and bodies.size() > 0 and bodies[0] is VehicleBody3D:
-		visible = 0
-		set_physics_process(false)
-		audio_stream_player.play()
-		var blackboard = get_node("/root/Blackboard")
-		if blackboard != null:
-			blackboard.kvps["dogecoin_collected"] = 1
+	if bodies != null:
+		for body in bodies:
+			if body is VehicleBody3D:
+				visible = 0
+				set_physics_process(false)
+				audio_stream_player.play()
+				var blackboard = get_node("/root/Blackboard")
+				if blackboard != null:
+					blackboard.kvps["dogecoin_collected"] = 1
